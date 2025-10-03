a111d8f45fab99c1f4f314b9cb9a3f5ca118a65a
Adam Fojtu <a.s.fojtu@gmail.com>
2025-10-03 16:40:53 +0200

    Make correction

--- pages/Get_Group_IDs.py
+++ pages/Get_Group_IDs.py
@@ -6,26 +6,29 @@ st.title("Get Group IDs")
 
 df = st.file_uploader("📁 Upload your keywords Excel file", type=["xlsx"])
 
-num_cols = df.columns.difference(["Keyword", "Total Volume"])
+if df:
+    st.success(f"Loaded {df.shape[0]} keywords.")
+    st.write(df.head())
+    num_cols = df.columns.difference(["Keyword", "Total Volume"])
 
-df["VolumePatternKey"] = df[num_cols].astype(str).agg('|'.join, axis=1)
+    df["VolumePatternKey"] = df[num_cols].astype(str).agg('|'.join, axis=1)
 
-# Assign GroupIDs in order of first appearance
-group_id_dict = {}
-group_id_counter = 1
-group_ids = []
+    # Assign GroupIDs in order of first appearance
+    group_id_dict = {}
+    group_id_counter = 1
+    group_ids = []
 
-for key in df["VolumePatternKey"]:
-    if key not in group_id_dict:
-        group_id_dict[key] = group_id_counter
-        group_id_counter += 1
-    group_ids.append(group_id_dict[key])
+    for key in df["VolumePatternKey"]:
+        if key not in group_id_dict:
+            group_id_dict[key] = group_id_counter
+            group_id_counter += 1
+        group_ids.append(group_id_dict[key])
 
-df["GroupID"] = group_ids
+    df["GroupID"] = group_ids
 
-# Drop helper column if not needed
-df.drop(columns=["VolumePatternKey"], inplace=True)
+    # Drop helper column if not needed
+    df.drop(columns=["VolumePatternKey"], inplace=True)
 
-filename = f"group_id_table {dt.datetime.now().strftime('%d-%m-%Y %H-%M-%S')}"
+    filename = f"group_id_table {dt.datetime.now().strftime('%d-%m-%Y %H-%M-%S')}"
 
-st.download_button("Download Table with Group IDs", df, file_name=filename)
+    st.download_button("Download Table with Group IDs", df, file_name=filename)
